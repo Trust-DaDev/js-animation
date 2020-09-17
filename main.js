76dbe4b8aa041a19e92140a92af16c9fa71588f6
Trust-coder1 <70828775+Trust-coder1@users.noreply.github.com>
2020-09-17 13:01:14 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// Add all your JS here
+function work() {
+    const work = document.getElementById("code")
+    work.innerHTML = "my work";
+    work.style.color = "red"
+    work.style.transform = "rotateZ(170deg)"
+    work.style.opacity = "1"
+    work.style.background = "linear-gradient(yellow,pink)"
+}
